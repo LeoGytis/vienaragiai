5e6451ff9197f9a6c6b586a4cd15a173a110a35c
LeoGytis <leogytis@gmail.com>
2022-05-09 15:54:05 +0300

    laikas

--- kintamieji.php
+++ kintamieji.php
@@ -55,8 +55,6 @@ $b = rand(1, 10);
 $c = rand(1, 10);
 
 echo "Krastiniu ilgiai: $a $b $c <br>";
-echo "<br>";
-
 
 if ($a + $b > $c 
     && $b + $c > $a 
@@ -76,34 +74,35 @@ $r4 = rand(0, 2);
 
 echo "Skaciai: $r1 $r2 $r3 $r4";
 echo "<br>";
-$nuliu = 0;
-$vienetu = 0;
-$dvejetu = 0;
 
+$c2 = 0;
 
-if ($r1 = 0 || $r2 = 0 || $r3 = 0 || $r4 = 0) {
-    $nuliu++;
+if ($r1 == 2) {
+    $c2++;
 }
-    else if ($r1 = 1 || $r2 = 1 || $r3 = 1 || $r4 = 1) {
-        $vienetu++;
-    }
-    else if ($r1 = 2 || $r2 = 2 || $r3 = 2 || $r4 = 2) {
-        $dvejetu++;
-    } 
+if ($r2 == 2) {
+    $c2++;
+}
+if ($r3 == 2) {
+    $c2++;
+}
+if ($r4 == 2) {
+    $c2++;
+}
+$c1 = $r1 + $r2 + $r3 + $r4 - ($c2 * 2);
+$c0 = 4 - $c1 - $c2;
 
-
-echo "Is viso nuliu: $nuliu <br>";
-echo "Is viso vienetu: $vienetu <br>";   
-echo "Is viso dvejetu: $dvejetu <br>";   
+echo "Is viso nuliu: $c0 <br>";
+echo "Is viso vienetu: $c1 <br>";   
+echo "Is viso dvejetu: $c2 <br>";   
 
 echo '<br><b> 6 Uzduotis </b><br>';
 
 $skaicius = rand(1, 6);
 echo "<h$skaicius>$skaicius</h$skaicius>";
-echo "<br>";
 
 
-echo '<br><b> 7 Uzduotis </b><br>';
+echo '<b> 7 Uzduotis </b><br>';
 
 for ($i = 0; $i < 3; $i++) {
     $x = rand(-10, 10);
@@ -113,7 +112,7 @@ for ($i = 0; $i < 3; $i++) {
         echo "<font color=blue>$x <br>";
     } else echo "<font color=red>$x <br>";
 }
-echo "<font color=black><br>";
+echo "<font color=black>";
 
 
 echo '<br><b> 8 Uzduotis </b><br>';
@@ -142,9 +141,59 @@ echo '<br><b> 9 Uzduotis </b><br>';
 $x1 = rand(0, 100);
 $x2 = rand(0, 100);
 $x3 = rand(0, 100);
+// $x1 = 10;
+// $x2 = 30;
+// $x3 = 95;
 echo "Skaiciai: $x1 $x2 $x3 <br>";
 
 $vidurkis = ($x1 + $x2 + $x3) / 3;
-echo round($vidurkis);
+echo 'Vidurkis: '. round($vidurkis);
+echo "<br>";
 
-if () {}
+$i9 = 0;
+$x11 = 0;
+$x22 = 0;
+$x33 = 0;
+
+if (10 <= $x1 && $x1 <= 90) {
+    $x11 = $x1;
+    $i9++;
+}
+if (10 <= $x2 && $x2 <= 90) {
+    $x22 = $x2;
+    $i9++;
+}
+if (10 <= $x3 && $x3 <= 90) {
+    $x33 = $x3;
+    $i9++;
+}
+
+$vidGeras = ($x11 + $x22 + $x33) / $i9;
+echo 'Vidurkio vidurkis: '. round($vidGeras);
+echo "<br>";
+
+echo '<br><b> 10 Uzduotis </b><br>';
+
+$valandos = rand(0, 23);
+$minutes = rand(0, 59);
+$sekundes = rand(0, 59);
+$extraSec = rand(0, 300); // 240sec = 4
+$extraMin = round($extraSec / 60);
+
+echo 'Laikas: '. $valandos. 'h. '. $minutes. 'min. '. $sekundes. 'sec.'. '<br>';
+echo 'Pridedamos sekundes: '. $extraSec. 'sec. = '. $extraMin. ' min.';
+
+if ($sekundes + $extraSec > 59) {
+    $minutes += round($extraSec / 60);
+    $sekundes += $sekundes % 60;
+}
+if ($minutes + $extraMin > 59) {
+    $valandos += 1;
+}
+
+
+$naujosMinutes = $minutes + $extraMin;
+$naujosSekundes = 
+
+
+$naujasLaikas = $minutes + round($extraSec / 30);
