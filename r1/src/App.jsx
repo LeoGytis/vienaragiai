7955a35cc533e6a41ede39b9fe825fffb7697c7e
LeoGytis <leogytis@gmail.com>
2022-05-19 14:28:35 +0300

    Commit

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,30 +1,51 @@
 import './App.css';
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
 
-    // const cats = ['Pilkis', 'Murka', 'Rainis'];
-    // const [kv, setKv] = useState([]);
+    const dogsArray = [ 'Bobikas', 'Lupis', 'Tūzikas', 'Au', 'Šamba', 'Šarikas'];
+    const [dogs, setDogs] = useState(dogsArray);
 
-    const dogs = ['Lupis', 'Tūzikas', 'Šamba', 'Šarikas', 'Bobikas'];
-    const [sunys, setSunys] = useState(dogs);
+    // const sortDogs = () => {
+    //     setDogs(dogy => dogy.slice(1)); // funkcija atimti viena elementa
+    // }
 
-    // const addKv = () => setSunys(dogs => [...dogs]); // funkcija prideti elementa i masyva
+    
 
-    const sortSunys = () => {
-        setSunys(dogs => dogs.slice(1)); // funkcija prideti elementa i masyva
+    const dogsSortFunction = (a, b) => a.length - b.length; 
+
+    const sortDogs = () => {
+        console.log(dogs);
+        let sortedDogs = dogs.sort(dogsSortFunction);
+        setDogs(sortedDogs);
+        console.log(dogs);
+        console.log('DOGSaaaaa');
     }
+    // const sortDogs = () => {
+    //     setDogs(dogs => dogs.sort(function(a,b){
+    //         b.length - a.length;
+    //         return b.length - a.length; 
+    //     }))
+    //     dogsArray.sort(function(a, b){
+    //         // let a = a(a.service.value);
+    //         // let b = b(b.service.value);
+    //         // a.length - b.length  // ASCENDING
+    //         b.length - a.length;     // DESCENDING
+    //         return b.length - a.length;
+          
+    // }
+    
    
     return (
       <div className="App">
         <header className="App-header">
             <h1>ND: REACT BASE LIST</h1>
-            <div className=''>
+            <div>
             {
-                dogs.map((dog,i) => <div key={i}>{dog}</div>)
+                dogs.map((dogs,i) => <div key={i}>{dogs}</div>)
             }
-            <button onClick={sortSunys}>Surikiuoti sunis!</button>
          </div>
+         <button onClick={sortDogs}>Surikiuoti sunis!</button>
         </header>
       </div>
     );
