172a504e9329454222fe4accefc17989883a95d6
LeoGytis <leogytis@gmail.com>
2022-05-19 11:52:16 +0300

    009 pamoka

---
+++ r1/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+
+    const dogs = ['Lupis', 'Šamba', 'Šarikas', 'Bobikas']
+   
+    return (
+      <div className="App">
+        <header className="App-header">
+            <h1>REACT BASE LIST</h1>
+        </header>
+      </div>
+    );
+}
+  
+export default App;
+  
