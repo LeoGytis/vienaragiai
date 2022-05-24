ec89897a0a7edb7a61de7b3c1b26777b289fcd84
LeoGytis <leogytis@gmail.com>
2022-05-24 08:40:03 +0300

    Class State

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -10,30 +10,56 @@ import { useState } from "react";
 // import Vandenynas from './ND/List/04-Uzdduotis/Vandenynas';
 
 function App() {
-  const [figura, setFigura] = useState("rutulys");
-  const [skaicius, setSkaicius] = useState(0);
 
+  //1 uzduotis keicia figura
+  const [figura, setFigura] = useState("rutulys");
   const changeFigure = () => {
     setFigura((oldFigure) =>
       oldFigure === "rutulys" ? "kvadratas" : "rutulys"  //tikrina className='{figura}'
     );
   };
 
-  //keicia {skaicius} propsa
+  // 2-3 uzduotis - keicia {skaicius} propsa
+  const [skaicius, setSkaicius] = useState(0);
   const randomNumber = () => setSkaicius(Math.floor(Math.random() * (25 - 5) ) + 5);
   const addNumber = () => setSkaicius((number) =>  number + 1);
   const minusNumber = () => setSkaicius((number) =>  number - 3);
+
+  // 4 uzduotis - prideda kvadratelius
+  const [kvadratas, setKvadratas] = useState([]);
+  const addCube = () => setKvadratas(kvadratas => [...kvadratas, null]);
+  const removeCube = () => setKvadratas(kvadratas => kvadratas.slice(1));
+
+  // 5 uzduotis - add red, add blue ir reset.
+  const [kv, setKv] = useState([]);
+  const addRed = (color) => setKv(kv => [...kv, color]);
+  const addBlue = (color) => setKv(kv => [...kv, color]);
+  const reset = () => setKv(kv => []);
   
 
   return (
     <div className="App">
       <header className="App-header">
         <div className={figura}>{skaicius}</div>
+        <div>
+          {
+            kvadratas.map((kv, i) => <div key={i} className="kvadratukas">{kv}</div> )
+          }
+        </div>
+        <div>
+          {
+            kv.map((color, i) => <div key={i} className="kvadratukas" style={{background: color}}>{null}</div> )
+          }
+        </div>
         <button onClick={changeFigure}>Change</button>
         <button onClick={randomNumber}>Random</button>
         <button onClick={addNumber}>Plus</button>
         <button onClick={minusNumber}>Minus</button>
         <button onClick={addCube}>Add</button>
+        <button onClick={removeCube}>Remove</button>
+        <button onClick={() => addRed('red')}>Add Red</button>
+        <button onClick={() => addBlue('blue')}>Add Blue</button>
+        <button onClick={reset}>Reset</button>
       </header>
     </div>
   );
