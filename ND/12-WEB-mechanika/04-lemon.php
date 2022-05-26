f616d3efa8c6e1300572ef3eb2c645ffb72d67e3
LeoGytis <leogytis@gmail.com>
2022-05-26 19:33:41 +0300

    Varom tolyn

---
+++ ND/12-WEB-mechanika/04-lemon.php
@@ -0,0 +1,18 @@
+<?php 
+
+header( "Location: http://localhost/vienaragiai/ND/12-WEB-mechanika/04-orange.php" );
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lemon</title>
+</head>
+<body style="background-color: yellow;">
+    Citrinu limonadas
+</body>
+</html>
