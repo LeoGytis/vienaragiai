b8a609f0e65c4051220a52362feadae60d0e77ee
LeoGytis <leogytis@gmail.com>
2022-06-02 11:38:09 +0300

    pfu

--- Paskaitos/015/Bebras.php
+++ Paskaitos/015/Bebras.php
@@ -1,12 +1,26 @@
 <?php
+// KLASES DARO ANTANAS
 class Bebras {
 
     public $tail = 'Long';   // tik deklaracija (jokiu funkciju ar skaiciavimu)
     private $age = 23;
+    private $name, $childrens; // jeigu to pacio galima surasyt daugiau kintamuju
 
+    // konstruktorius veikia kiekviena objekta atksirai (pvz. rand sugeneruoja kitoki)
+    public function __construct(string $n, array $c)  //pasileidizia kiekviena karta kai kuriame nauja objekta su "new"
+    {
+        echo '<br>Construct pasileido<br>';
+        $this->whatIsYourAge();
+        $this->age = rand(1,100); //galima rasyti funkcijas
+        $this->whatIsYourAge();
+
+        $this->name = $n;
+        $this->childrends = $c;
+    }
+    
     public function whatIsYourAge() {  //getter
         echo '<br>' . 'Labas' . '<br>';
-        echo '<br>' . $this->age . '<br>';
+        echo '<br>' . $this->age;  
     }
 
     public function changeAge(int $age) {  //setter  // int $age - strong type (turi butinai paduot tokio tipo reiksme)
@@ -14,12 +28,30 @@ class Bebras {
             return;
         }
         $this->age = $age;
+    }
+
+     //Kai kreipemes uz matomumo ribu klase paleidzia get methoda
+    public function __get($what)
+     {  
+        if (in_array($what, ['age', 'name'])) {
+            return $this->$what;   //STIPRIAI ATKREIPT DEMESI KAD CIA YRA $$$ ($what)
+            
+        }
+        echo '<br>Magic __get ===== ' . $what . '<br>';
+        return '<br>Nieko nera namie<br>';
+        
+    }
+
+    public function __set($where, $what)
+    {
+        echo '<br>Magic __set metodas<br>';
+        echo $where . '----' . $what . '<br>';
 
 
     }
 
-
-
-
-
+    public function __destruct() //pasileidzia pabaigoje (iraso ka nors ir panasiai)
+    {
+        echo '<br>Magic destructorius<br>';
+    }
 }
--- Paskaitos/015/index.php
+++ Paskaitos/015/index.php
@@ -1,14 +1,14 @@
 <?php
-
+// NAUDOJA PRANAS
 require __DIR__ . '/Bebras.php';
 //klase nera kintamasis, ji yra popieriukas
 //klase nera is didziosios
 //objektas is didziosios
 
-$bebras1 = new Bebras;
-$bebras2 = new Bebras;
-$bebras3 = $bebras1; // tas pats objetas
-$bebras4 = clone($bebras1); // klonas bet jau kaip kitas objektas
+$bebras1 = new Bebras('Jonas', [0]);
+// $bebras2 = new Bebras('Janina', [1,2,3]);
+// $bebras3 = $bebras1; // tas pats objetas
+// $bebras4 = clone($bebras1); // klonas bet jau kaip kitas objektas
 
 echo '<pre>';
 
@@ -16,14 +16,19 @@ echo '<pre>';
 // var_dump($bebras2);
 // var_dump($bebras3);
 
-echo $bebras1->tail;  //tail be dolerio zenklo
-echo '<br>';
-$bebras1->tail = 'Very long';
-echo $bebras1->tail;  
+// echo $bebras1->tail;  //tail be dolerio zenklo
+// echo '<br>';
+// $bebras1->tail = 'Very long';
+// echo $bebras1->tail;  
+
+
+echo $bebras1->belenkas;  //Get metodas gauna visas reiksmes
+echo $bebras1->age;  
+echo $bebras1->name;  
 
 
 // echo $bebras1->age;  //privatus todel nerodo
 
-$bebras1->changeAge(26); //iskviesti funkcija klaseje
+// $bebras1->changeAge(26); //iskviesti funkcija klaseje
 
-$bebras1->whatIsYourAge(); //iskviesti funkcija klaseje
+// $bebras1->whatIsYourAge(); //iskviesti funkcija klaseje
