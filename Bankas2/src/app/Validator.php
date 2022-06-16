ee3d56a1229343c8a92255d5c452475421cd15bf
LeoGytis <leogytis@gmail.com>
2022-06-16 13:08:47 +0300

    validation edit

--- Bankas2/src/app/Validator.php
+++ Bankas2/src/app/Validator.php
@@ -15,42 +15,39 @@ class Validator
         $this->data = json_decode(file_get_contents(__DIR__ . '/../data/' . $file . '.json'), true);
     }
 
-    private function socialIDcheck(int $id)
+    private function isSameSocialId(int $id)
     {
         foreach ($this->data as $data) {
             if ($data['social_id'] == $id) {
-                return true;
+                return false;
                 break;
             }
         }
     }
 
-    private function nameLengthCheck($name, $surname)
+    private function isNameLengthValid($name, $surname)
     {
-        if (strlen($name) <= 3 || strlen($surname) <= 3) {
-            return true;
-        } else return false;
-        // strlen($name) <= 3 ? true : (strlen($surname) <= 3) ? true : false;   // kaip parasyti ternanry???
+        return strlen($name) <= 3 || strlen($surname) <= 3 ? false : true;
     }
 
-    private function socialIDvalidation(int $id)
+    private function isSocialIdValid(int $id)
     {
         if ((int)substr($id, 0, 1) != 3 && (int)substr($id, 0, 1) != 4 || (int)substr($id, 3, 2) > 12 || (int)substr($id, 5, 2) > 31 || (int)strlen($id) != 11) {
-            return true;
-        } else return false;
+            return false;
+        } else return true;
     }
 
     public function formValidation($post_data)
     {
-        if (self::nameLengthCheck($post_data['name'], $post_data['surname'])) {
+        if (!self::isNameLengthValid($post_data['name'], $post_data['surname'])) {
             M::add('Vardas ir pavardė turi turėti bent 4 simbolius.', 'alert');
             return true;
         }
-        if (self::socialIDcheck($post_data['social_id'])) {
+        if (!self::isSameSocialId($post_data['social_id'])) {
             M::add('Toks asmens kodas jau yra.', 'alert');
             return true;
         }
-        if (self::socialIDvalidation($post_data['social_id'])) {
+        if (!self::isSocialIdValid($post_data['social_id'])) {
             M::add('Netinkamai įvestas asmens kodas', 'alert');
             return true;
         }
