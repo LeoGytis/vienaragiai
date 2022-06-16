8dcc13e653018ce89de91ed30c9aec7364d42ec2
LeoGytis <leogytis@gmail.com>
2022-06-16 12:53:11 +0300

    react

--- react2/src/Components/List.jsx
+++ react2/src/Components/List.jsx
@@ -1,11 +1,24 @@
+import { useContext } from "react";
+import DataContext from "./DataContext";
+
 function List() {
+  const { animals } = useContext(DataContext);
+
   return (
     <div className="col-7">
       <div className="card mt-4">
         <div className="card-header">
           <h2>List</h2>
         </div>
-        <div className="card-body"></div>
+        <div className="card-body">
+          <ul class="list-group">
+            {animals.map((a) => (
+              <li class="list-group-item" key="{a.id}>{a.animal}">
+                An item
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
