b2586aead35290f5bcabc835cba040b5d31e72e4
LeoGytis <leogytis@gmail.com>
2022-06-29 10:27:33 +0300

    viskas ok

--- db/index.php
+++ db/index.php
@@ -20,9 +20,9 @@ $pdo = new PDO($dsn, $user, $pass, $options);
 <fieldset>
     <legend>CREATE</legend>
     <form method="post">
-        Title <input type="text" name="title">
-        Height <input type="text" name="height">
-        Type <select name="type">
+        Title: <input type="text" name="title">
+        Height: <input type="text" name="height">
+        Type: <select name="type">
             <option value="1">Lapas</option>
             <option value="2">Spyglys</option>
             <option value="3">Palme</option>
@@ -32,21 +32,88 @@ $pdo = new PDO($dsn, $user, $pass, $options);
     </form>
 </fieldset>
 
+<fieldset>
+    <legend>DELETE</legend>
+    <form method="post">
+        ID: <input type="text" name="id">
+        <input type="hidden" name="_method" value="delete">
+        <button type="submit">Delete</button>
+    </form>
+</fieldset>
+
+<fieldset>
+    <legend>UPDATE</legend>
+    <form method="post">
+        ID: <input type="text" name="id">
+        Title: <input type="text" name="title">
+        <input type="hidden" name="_method" value="put">
+        <button type="submit">Update</button>
+    </form>
+</fieldset>
+
 
 <?php
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    // CREATE
+
+    // ============= CREATE ============= 
+    // INSERT INTO table_name (column1, column2, column3, ...)
+    // VALUES (value1, value2, value3, ...);
     if ($_POST['_method'] == 'post') {
         $sql = "
         INSERT INTO trees
         (title, height, type)
-        VALUES ('" . $_POST['title'] . "'," . $_POST['height'] . "," . $_POST['type'] . ")
+        VALUES (:a, :z, :type)
         ";
+        // Situs buvo galima rasyt bet daug kabuciu
+        // VALUES ('" . $_POST['title'] . "'," . $_POST['height'] . "," . $_POST['type'] . ")
+        // $pdo->query($sql); 
+        $stmt = $pdo->prepare($sql);
+        // $stmt->execute([$_POST['title'], $_POST['height'], $_POST['type']]);
+
+        $stmt->execute([
+            'z' => $_POST['height'],
+            'type' => $_POST['type'],
+            'a' => $_POST['title']
+        ]);
+
+
+        header('Location: http://localhost/vienaragiai/db/');
+        die;
     }
-    $pdo->query($sql);
-    header('Location: http://localhost/vienaragiai/db/');
-    die;
+
+    // ============= DELETE ============= 
+    // DELETE FROM table_name WHERE condition;
+    if ($_POST['_method'] == 'delete') {
+
+        $sql = "
+            DELETE FROM trees
+            WHERE id = ?
+        ";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([$_POST['id']]);
+
+        header('Location: http://localhost/vienaragiai/db/');
+        die;
+    }
+
+
+    // ============= UPDATE ============= 
+    // UPDATE table_name
+    // SET column1 = value1, column2 = value2, ...
+    // WHERE condition;
+    if ($_POST['_method'] == 'put') {
+        $sql = "
+            UPDATE trees
+            SET title = ?
+            WHERE id = ?
+        ";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([$_POST['title'], $_POST['id']]);
+
+        header('Location: http://localhost/vienaragiai/db/');
+        die;
+    }
 }
 
 
@@ -94,8 +161,7 @@ $sql = "
 $stmt = $pdo->query($sql);
 
 $trees = $stmt->fetchAll();
-print_r($trees);
-
+// print_r($trees);
 
 // echo $tree['height_sum'];
 
