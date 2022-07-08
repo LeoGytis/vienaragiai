99fea81480944c7abde307234898bed472a973ad
LeoGytis <leogytis@gmail.com>
2022-07-08 14:59:26 +0300

    ok ok

--- bankas_l4/resources/views/client/edit.blade.php
+++ bankas_l4/resources/views/client/edit.blade.php
@@ -9,7 +9,7 @@
         </div>
         <div class="card-body">
             <div class="form-group">
-                <div class="alert alert-info text-center"></div>
+                <div class="alert card-header thin-line" role="alert"></div>
                 <form action="{{route('clients-update', $client)}}" method="post">
                     <label>Name:</label>
                     <input class="form-control mb-3" type="text" name="name_input" value="{{$client->name}}">
@@ -19,9 +19,9 @@
                     <input class="form-control mb-3" type="text" name="social_id_input" value="{{$client->social_id}}">
                     @csrf
                     @method('put')
-                    <button class="col-3 btn btn-info mt-4 mb-4" type="submit">Edit</button>
+                    <div class="alert card-header thin-line mt-4" role="alert"></div>
+                    <button class="col-3 btn btn-info card-header mt-4 mb-4" type="submit">Edit</button>
                 </form>
-                <div class="alert alert-info text-center"></div>
             </div>
         </div>
     </div>
